1d9b1f796cb2ac885ab708c3ebe586456be1eb31
Jacob <jacobhtnguyen@gmail.com>
2019-08-17 18:13:29 -0400

    updated middleware consumer to take in a controller instead of path

--- 01-cats-app/src/app.module.ts
+++ 01-cats-app/src/app.module.ts
@@ -1,8 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
@@ -12,7 +13,7 @@ import { CatsModule } from './cats/cats.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      .apply(LoggerMiddleware) // apply can take multiple middleware functions
+      .forRoutes(CatsController); // forRoutes can take multiple strings and controllers
   }
 }
