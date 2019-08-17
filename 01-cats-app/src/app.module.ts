f4b226d1c857b4d0e423538ece6e0c5512fe6c81
Jacob <jacobhtnguyen@gmail.com>
2019-08-17 18:31:03 -0400

    created a functional middleware and switched from class middleware logger to functional middleware logger

--- 01-cats-app/src/app.module.ts
+++ 01-cats-app/src/app.module.ts
@@ -1,7 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { logger, LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
 
@@ -13,10 +13,18 @@ import { CatsController } from './cats/cats.controller';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(LoggerMiddleware) // apply can take multiple middleware functions
-      .exclude(
-        { path: 'cats', method: RequestMethod.GET }, // excludes the get methods
-      )
-      .forRoutes(CatsController); // forRoutes can take multiple strings and controllers
+      .apply(logger) // can apply multiple middleware ex. .apply(cors(), helmet(), logger)
+      .forRoutes(CatsController);
   }
 }
+
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(LoggerMiddleware) // apply can take multiple middleware functions
+//       .exclude(
+//         { path: 'cats', method: RequestMethod.GET }, // excludes the get methods
+//       )
+//       .forRoutes(CatsController); // forRoutes can take multiple strings and controllers
+//   }
+// }
--- 01-cats-app/src/common/middleware/logger.middleware.ts
+++ 01-cats-app/src/common/middleware/logger.middleware.ts
@@ -6,7 +6,13 @@ export class LoggerMiddleware implements NestMiddleware {
   // tslint:disable-next-line:ban-types
   use(req: Request, res: Response, next: Function) {
     // tslint:disable-next-line:no-console
-    console.log('Request...' + req.method + req.baseUrl);
+    console.log('ClassLogger: Request...' + req.method + req.baseUrl);
     next();
   }
+}
+
+export function logger(req, res, next) {
+  // tslint:disable-next-line:no-console
+  console.log('FunctionalLogger: Request...' + req.method + req.baseUrl);
+  next();
 }
